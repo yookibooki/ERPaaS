8707ffdab7fc5e347ca655476cd54ec61cfb3bec
John Gray <rendogust@gmail.com>
2025-02-10 16:54:15 +0500

    updated

--- app.go
+++ app.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
